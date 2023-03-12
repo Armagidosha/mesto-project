141176138ee43d0e46bfe390e2ce1ecd08752c55
Vladimir Z <armgidosha@list.ru>
2023-03-12 18:24:06 +0300

    refactor: add new function addCard, rename addCard > createCard

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 // Constants
+const cardTemplate = document.querySelector('#card-Template').content;
 const authorName = document.querySelector('.profile__author');
 const authorDescription = document.querySelector('.profile__description');
-const cardsList = document.querySelector('.elements__list');
+const cardsContainer = document.querySelector('.elements__list');
 // Popup Image
-const popupImagePreview = document.querySelector('.popup_image_preview');
-const popupImageCloseButton = document.querySelector('.popup__close-button');
+const popupImagePreview = document.querySelector('.popup_type_preview');
+const imagePreview = document.querySelector('.popup__image');
+const imageFigCaption = document.querySelector('.popup__figcaption');
 // Popup edit 
-const popupEdit = document.querySelector('.popup_profile_edit');
+const popupEdit = document.querySelector('.popup_type_edit');
 const profileEditButton = document.querySelector('.profile__edit-button');
-const popupEditCloseButton = popupEdit.querySelector('.popup__close-button');
 const submitEditForm = popupEdit.querySelector('.popup__form')
 const inputName = popupEdit.querySelector('.popup__input_type_name');
 const inputDescription = popupEdit.querySelector('.popup__input_type_description');
 // Popup add 
-const popupAdd = document.querySelector('.popup_card_add');
+const popupAdd = document.querySelector('.popup_type_add');
 const cardAddButton = document.querySelector('.profile__add-button');
-const popupAddCloseButton = popupAdd.querySelector('.popup__close-button');
 const submitAddForm = popupAdd.querySelector('.popup__form')
 // Popup open / close functions
 function openPopup (popup) {
@@ -24,24 +24,22 @@ function openPopup (popup) {
 function closePopup (popup) {
   popup.classList.remove('popup_opened');
 }
-// Popup's Open / close 
+// Popup open button
 profileEditButton.addEventListener('click', () => {
   openPopup(popupEdit);
   inputName.value = authorName.textContent; // Убрать после полного завершения проекта 
   inputDescription.value = authorDescription.textContent; // Убрать после полного завершения проекта
 });
-popupEditCloseButton.addEventListener('click', () => {
-  closePopup(popupEdit)
-});
 cardAddButton.addEventListener('click', () => {
   openPopup(popupAdd)
+  headingInput.value = '';
+  imageInput.value = '';
 });
-popupAddCloseButton.addEventListener('click', () => {
-  closePopup(popupAdd)
-});
-popupImageCloseButton.addEventListener('click', () => {
-  closePopup(popupImagePreview);
-})
+// Popup close butons
+document.querySelectorAll('.popup__close-button').forEach(button => {
+  const buttonsPopup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(buttonsPopup));
+});  
 // Submit profile button
 function submitProfile (evt) {
   evt.preventDefault();
@@ -52,19 +50,16 @@ function submitProfile (evt) {
 submitEditForm.addEventListener('submit', submitProfile);
 // Image Popup Open 
 function openImagePreview(imageUrl, figCap) {
-  const imagePreview = document.querySelector('.popup__image');
-  const imageFigCaption = document.querySelector('.popup__figcaption');
   openPopup(popupImagePreview);
   imagePreview.src = imageUrl;
   imagePreview.alt = figCap;
   imageFigCaption.textContent = figCap;
 }
-//  Add Cards function
+//  Create card function
 const headingInput = document.querySelector('.popup__input_type_card-heading');
 const imageInput = document.querySelector('.popup__input_type_card-url');
 
-function addCard(name, link) {
-  const cardTemplate = document.querySelector('#card-Template').content;
+function createCard(name, link) {
   const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
   const image = cardElement.querySelector('.element__image');
   const heading = cardElement.querySelector('.element__heading');
@@ -74,53 +69,28 @@ function addCard(name, link) {
   cardElement.querySelector('.element__delete-button').addEventListener('click', evt => {
     evt.target.closest('.element').remove();
   });
-  cardsList.prepend(cardElement);
+  cardsContainer.prepend(cardElement);
   heading.textContent = name;
   image.src = link;
   image.alt = name;
   image.addEventListener('click', () => {
     openImagePreview(link, name)
   });
-  headingInput.value = '',
-  imageInput.value = '',
   closePopup(popupAdd);
-  console.log('Card Created ' + name);
+  return cardElement;
 }
+// Add card Funcion
+function addCard (name, link) {
+  cardsContainer.prepend(createCard(name, link))
+};
 // Card submit button
 submitAddForm.addEventListener('submit', () => {
   addCard(headingInput.value, imageInput.value);
 });
-// 
-const initialCards = [
-  {
-    name: 'Канада, озеро Морейн',
-    link: 'https://co19.nevseoboi.com.ua/mountains/331/33123/1562510903-1005339-mountains-nevseoboi.com.ua.jpg'
-  },
-  {
-    name: 'Байкал, остров Ольхон',
-    link: 'https://images.unsplash.com/photo-1676466920684-5d1aae90c9c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-  },
-  {
-    name: 'Италия, Моэна',
-    link: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-  },
-  {
-    name: 'США, Чикаго',
-    link: 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-  },
-  {
-    name: 'Ёжик, еловый лес',
-    link: 'https://images.unsplash.com/photo-1622227056993-6e7f88420855?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-  },
-  {
-    name: 'Канада, Ниагарский водопад',
-    link: 'https://images.unsplash.com/photo-1511358146320-eb018ab3e22e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-  }
-]; 
 // Auto-add cards on page load 
-function dataAddCards () {
+function addArrayCards () {
 for (let i = 0; i < initialCards.length; i+=1) {
   addCard(initialCards[i].name, initialCards[i].link)
   }
 }
-dataAddCards()
+addArrayCards()
