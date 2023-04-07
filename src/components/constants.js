9f0b5be810a6d19fa51bc30646b3379b559ddf3d
Vladimir Z <armgidosha@list.ru>
2023-04-07 16:34:57 +0300

    refactor: add cardAddButton constant

--- src/components/constants.js
+++ src/components/constants.js
@@ -16,6 +16,7 @@ export const inputDescription = popupEdit.querySelector('.popup__input_type_desc
 export const popupAdd = document.querySelector('.popup_type_add');
 export const cardAddButton = document.querySelector('.profile__add-button');
 export const submitAddForm = popupAdd.querySelector('.popup__form')
+export const addSubmitButton = popupAdd.querySelector('.popup__save-button')
 
 export const headingInput = document.querySelector('.popup__input_type_card-heading');
 export const imageInput = document.querySelector('.popup__input_type_card-url');
