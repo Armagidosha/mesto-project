c5f85e83d7da94e0e4b7c1d8ead646cb8b539222
Vladimir Z <armgidosha@list.ru>
2023-04-07 16:34:10 +0300

    refactor: rewrote some functions

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,6 @@
 import './../pages/index.css'
-import {cardTemplate, 
+import {
   authorName, authorDescription, 
-  cardsContainer, 
-  popupImagePreview, 
-  imagePreview, 
-  imageFigCaption, 
   popupEdit, profileEditButton, 
   submitEditForm, 
   inputName, 
@@ -14,13 +10,14 @@ import {cardTemplate,
   headingInput,
   imageInput, 
   submitAddForm,
+  addSubmitButton,
 } from './constants';
 import {closePopup, openPopup} from './modal';
-import {initialCards} from './initialCards';
+import {addArrayCards, addCard} from './card';
 import {formConst} from './utils.js';
-import {enableValidation} from './validate';
+import {enableValidation, resetValidation} from './validate';
 
-function CloseButtonsListener () {
+function сloseButtonsListener () {
   document.querySelectorAll('.popup__close-button').forEach(button => {
     const buttonsPopup = button.closest('.popup');
     button.addEventListener('click', () => closePopup(buttonsPopup));
@@ -30,6 +27,7 @@ function CloseButtonsListener () {
 // Открытие попапа редактирования профиля
 profileEditButton.addEventListener('click', () => {
   openPopup(popupEdit);
+  resetValidation(popupEdit, formConst);
   inputName.value = authorName.textContent; // Убрать после полного завершения проекта 
   inputDescription.value = authorDescription.textContent; // Убрать после полного завершения проекта
 });
@@ -37,8 +35,10 @@ profileEditButton.addEventListener('click', () => {
 // Открытие попапа добавления карточек
 cardAddButton.addEventListener('click', () => {
   openPopup(popupAdd)
-  headingInput.value = '';
-  imageInput.value = '';
+  resetValidation(popupAdd, formConst);
+  submitAddForm.reset();
+  addSubmitButton.disabled = true;
+  addSubmitButton.classList.add('popup__save-button_disabled');
 });
 
 // Функция сабмита профиля
@@ -50,56 +50,13 @@ function submitProfile (evt) {
 }
 submitEditForm.addEventListener('submit', submitProfile);
 
-// Открытие попапа с картинкой
-function openImagePreview(imageUrl, figCap) {
-  openPopup(popupImagePreview);
-  imagePreview.src = imageUrl;
-  imagePreview.alt = figCap;
-  imageFigCaption.textContent = figCap;
-}
-
-// Функция создания карточек
-function createCard(name, link) {
-  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
-  const image = cardElement.querySelector('.element__image');
-  const heading = cardElement.querySelector('.element__heading');
-  cardElement.querySelector('.element__like-button').addEventListener('click', evt => {
-    evt.target.classList.toggle('element__like-button_active')
-  });
-  cardElement.querySelector('.element__delete-button').addEventListener('click', evt => {
-    evt.target.closest('.element').remove();
-  });
-  heading.textContent = name;
-  image.src = link;
-  image.alt = name;
-  image.addEventListener('click', () => {
-    openImagePreview(link, name)
-  });
-  closePopup(popupAdd);
-  return cardElement;
-}
-
-// Функция добавления карточек
-function addCard (name, link) {
-  cardsContainer.prepend(createCard(name, link))
-};
-
 // Вешаем слушатель на форму добавления карточек
 submitAddForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   addCard(headingInput.value, imageInput.value);
+  closePopup(popupAdd);
 });
 
-// Добавляем карточки при загрузке страницы
-function addArrayCards () {
-  for (let i = 0; i < initialCards.length; i+=1) {
-    addCard(initialCards[i].name, initialCards[i].link)
-  }
-}
 addArrayCards();
 enableValidation(formConst);
-CloseButtonsListener();
-
-
-
-
+сloseButtonsListener();
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,28 +1,24 @@
 import {popupList} from "./constants";
-import {resetValidation} from "./validate";
-import {formConst} from "./utils";
 
 // Открытие попапа
 export function openPopup (popup) {
   popup.classList.add('popup_opened');
-  closePopupByEscape(popup);
-  resetValidation(popup, formConst);
+  document.addEventListener('keydown', closeByEscape);
 };
 
 // Закрытие попапа
 export function closePopup (popup) {
   popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closePopupByEscape)
+  document.removeEventListener('keydown', closeByEscape); 
 }
 
 // Вешаем обработчик на открытый нами попап, который позваляет закрывать его по нажатию на Esc
-function closePopupByEscape (popup) {
-  document.addEventListener('keyup', (evt) => {
-    if (evt.key === 'Escape') {
-      closePopup(popup)
-    }
-  })
-};
+function closeByEscape(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened');
+    closePopup(openedPopup);
+  }
+}
 
 // Проверяем является ли элемент на который мы кликаем "Попапом", если да, то вызывает функцию закрытия
 popupList.forEach(element => (
