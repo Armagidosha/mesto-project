295321068a17ea74588f1c2c98f47391223982bc
Vladimir Z <armgidosha@list.ru>
2023-04-10 15:39:23 +0300

    feat: add interaction with server

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,7 @@
-import {initialCards} from './initialCards';
 import {cardsContainer, cardTemplate, imagePreview, imageFigCaption, popupImagePreview} from './constants';
 import {openPopup} from './modal';
+import { getId} from '.';
+import { deleteCard, updateLikes } from './api';
 
 // Открытие попапа с картинкой
 function openImagePreview(imageUrl, figCap) {
@@ -10,20 +11,52 @@ function openImagePreview(imageUrl, figCap) {
   imageFigCaption.textContent = figCap;
 }
 
+const showLikeCount = (likes, cardElement, userId) => {
+  const likeButton = cardElement.querySelector('.element__like-button');
+  const likeCounter = cardElement.querySelector('.element__like-count');
+
+  likeCounter.textContent = `${likes.length}`;
+  const isLike = likes.some((item) => item._id === userId);
+  likeButton.classList.toggle('element__like-button_active', isLike);
+}
+
+const updLikes = (id, userId, cardElement) => {
+  const likeButton = cardElement.querySelector('.element__like-button');
+  const like = likeButton.classList.contains('element__like-button_active');
+  updateLikes(!like, id)
+  .then((card) => {
+    showLikeCount(card.likes, cardElement, userId);
+  })
+  .catch((error) => console.error(error))
+}
+
 // Функция создания карточек
-export function createCard(name, link) {
+export function createCard({name, link, likes, _id, owner}) {
   const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
   const image = cardElement.querySelector('.element__image');
   const heading = cardElement.querySelector('.element__heading');
-  cardElement.querySelector('.element__like-button').addEventListener('click', evt => {
-    evt.target.classList.toggle('element__like-button_active')
+  const removeButton = cardElement.querySelector('.element__delete-button')
+  const userId = getId();
+  cardElement.querySelector('.element__like-button').addEventListener('click', () => {
+    updLikes(_id, userId, cardElement);
   });
-  cardElement.querySelector('.element__delete-button').addEventListener('click', evt => {
-    evt.target.closest('.element').remove();
+  cardElement.querySelector('.element__delete-button').addEventListener('click', () => {
+    deleteCard(_id)
+    .then (() => {
+      cardElement.remove()
+    })
+    .catch((error) => console.error(error))
   });
   heading.textContent = name;
   image.src = link;
   image.alt = name;
+
+  if (owner._id !== userId) {
+    removeButton.remove()
+  }
+
+  showLikeCount(likes, cardElement, userId)
+
   image.addEventListener('click', () => {
     openImagePreview(link, name)
   });
@@ -31,13 +64,8 @@ export function createCard(name, link) {
 }
 
 // Функция добавления карточек
-export function addCard (name, link) {
-  cardsContainer.prepend(createCard(name, link))
+export const addCard = (cards) => {
+  cards.forEach((element) => {
+    cardsContainer.prepend(createCard(element))
+  })
 };
-
-// Добавляем карточки при загрузке страницы
-export function addArrayCards () {
-  for (let i = 0; i < initialCards.length; i+=1) {
-    addCard(initialCards[i].name, initialCards[i].link)
-  }
-}
--- src/components/index.js
+++ src/components/index.js
@@ -11,11 +11,19 @@ import {
   imageInput, 
   submitAddForm,
   addSubmitButton,
+  authorAvatar,
+  cardsContainer,
+  avatar,
+  popupAvatar,
+  avatarButton,
+  inputAvatarUrl,
+  avatarForm
 } from './constants';
 import {closePopup, openPopup} from './modal';
-import {addArrayCards, addCard} from './card';
+import {addCard, createCard} from './card';
 import {formConst} from './utils.js';
 import {enableValidation, resetValidation} from './validate';
+import {getUserInfo, getCards, postCards, patchUserInfo, updateAvatar} from './api';
 
 function сloseButtonsListener () {
   document.querySelectorAll('.popup__close-button').forEach(button => {
@@ -23,6 +31,15 @@ function сloseButtonsListener () {
     button.addEventListener('click', () => closePopup(buttonsPopup));
   });  
   }
+
+const renderLoading = (isLoading, form) => {
+  const button = form.querySelector('.popup__save-button')
+  if (isLoading) {
+    button.textContent = 'Сохранение...';
+  } else {
+    button.textContent = 'Сохранить';
+  }
+}
 
 // Открытие попапа редактирования профиля
 profileEditButton.addEventListener('click', () => {
@@ -41,22 +58,97 @@ cardAddButton.addEventListener('click', () => {
   addSubmitButton.classList.add('popup__save-button_disabled');
 });
 
+// Открытие попапа обновления аватара 
+avatar.addEventListener('click', () => {
+  openPopup(popupAvatar)
+  resetValidation(popupAvatar, formConst);
+  avatarForm.reset();
+  avatarButton.disabled = true;
+  avatarButton.classList.add('popup__save-button_disabled');
+})
+
+// Функция сабмита аватара
+function submitAvatar (evt) {
+  evt.preventDefault();
+  renderLoading(true, popupAvatar);
+  updateAvatar(inputAvatarUrl.value)
+  .then((res) => {
+    updUserInfo(res);
+    closePopup(popupAvatar);
+  })
+  .catch((error) => console.error(`Не удалось обновить аватар: ${error}`))
+  .finally(() => renderLoading(false, popupAvatar))
+}
+avatarForm.addEventListener('submit', submitAvatar)
+
 // Функция сабмита профиля
 function submitProfile (evt) {
   evt.preventDefault();
-  authorName.textContent = inputName.value;
-  authorDescription.textContent = inputDescription.value;
-  closePopup(popupEdit);
+  renderLoading(true, popupEdit)
+  patchUserInfo({name: inputName.value, about: inputDescription.value})
+  .then((res) => {
+    updUserInfo(res);
+    closePopup(popupEdit);
+  })
+  .catch((error) => console.error(`Не удалось изменить данные профиля: ${error}`))
+  .finally(() => renderLoading(false, popupEdit))
 }
 submitEditForm.addEventListener('submit', submitProfile);
 
-// Вешаем слушатель на форму добавления карточек
-submitAddForm.addEventListener('submit', (evt) => {
+// функция сабмита карточек
+const submitCards = (evt) => {
   evt.preventDefault();
-  addCard(headingInput.value, imageInput.value);
-  closePopup(popupAdd);
-});
-
-addArrayCards();
+  renderLoading(true, popupAdd);
+  postCards({
+    name: headingInput.value,
+    url: imageInput.value
+  })
+  .then((card) => {
+    cardsContainer.append(createCard(card))
+    closePopup(popupAdd);
+  })
+  .catch((error) => console.error(`Не удалось отправить карточку: ${error}`))
+  .finally(() => renderLoading(false, popupAdd))
+}
+submitAddForm.addEventListener('submit', submitCards)
+// 
+// addArrayCards();
 enableValidation(formConst);
 сloseButtonsListener();
+
+// 
+// 
+// 
+// A P I
+// 
+// 
+// 
+
+export const config = {
+  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-22',
+  headers: {
+    authorization: '51223542-913b-4f70-95e2-80679cf69654',
+    'Content-Type': 'application/json'
+  }
+}
+
+export const getId = () => {
+  return id;
+}
+
+let id = 0
+
+// Обновляем информацию на клиентской части
+const updUserInfo = ({name, about, avatar, _id}) => {
+  authorName.textContent = name;
+  authorDescription.textContent = about
+  authorAvatar.style.backgroundImage = `url(${avatar})`;
+  id = _id
+}
+
+Promise.all([getUserInfo(), getCards()])
+.then(([userInfo, cards]) => {
+  updUserInfo(userInfo);
+  addCard(cards)
+})
+.catch((error) => console.error(error))
