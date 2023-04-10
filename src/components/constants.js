91ade122a45c810e2c3d61a87a215c30ada8a2c6
Vladimir Z <armgidosha@list.ru>
2023-04-10 15:42:34 +0300

    refactor: add avatar prop exports

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,5 +1,6 @@
 export const cardTemplate = document.querySelector('#card-Template').content;
 export const authorName = document.querySelector('.profile__author');
+export const authorAvatar = document.querySelector('.profile__avatar');
 export const authorDescription = document.querySelector('.profile__description');
 export const cardsContainer = document.querySelector('.elements__list');
 
@@ -7,16 +8,23 @@ export const popupImagePreview = document.querySelector('.popup_type_preview');
 export const imagePreview = document.querySelector('.popup__image');
 export const imageFigCaption = document.querySelector('.popup__figcaption');
 
+export const avatar = document.querySelector('.profile__avatar')
+export const popupAvatar = document.querySelector('.popup_type_avatar');
+export const avatarButton = popupAvatar.querySelector('.popup__save-button');
+export const inputAvatarUrl = popupAvatar.querySelector('.popup__input_type_avatar-url');
+export const avatarForm = popupAvatar.querySelector('.popup__form');
+
+
 export const popupEdit = document.querySelector('.popup_type_edit');
 export const profileEditButton = document.querySelector('.profile__edit-button');
-export const submitEditForm = popupEdit.querySelector('.popup__form')
+export const submitEditForm = popupEdit.querySelector('.popup__form');
 export const inputName = popupEdit.querySelector('.popup__input_type_name');
 export const inputDescription = popupEdit.querySelector('.popup__input_type_description');
 
 export const popupAdd = document.querySelector('.popup_type_add');
 export const cardAddButton = document.querySelector('.profile__add-button');
-export const submitAddForm = popupAdd.querySelector('.popup__form')
-export const addSubmitButton = popupAdd.querySelector('.popup__save-button')
+export const submitAddForm = popupAdd.querySelector('.popup__form');
+export const addSubmitButton = popupAdd.querySelector('.popup__save-button');
 
 export const headingInput = document.querySelector('.popup__input_type_card-heading');
 export const imageInput = document.querySelector('.popup__input_type_card-url');
