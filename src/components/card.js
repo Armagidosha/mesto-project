946566531fd59591e9dff44f889be370f3d149a7
Vladimir Z <armgidosha@list.ru>
2023-04-27 15:16:52 +0300

    refactor: change card (OOP)

--- src/components/card.js
+++ src/components/card.js
@@ -3,60 +3,8 @@ import {openPopup} from './modal';
 import {getId} from './index.js';
 import {api} from './api';
 
-// // Открытие попапа с картинкой
-// function openImagePreview(imageUrl, figCap) {
-//   openPopup(popupImagePreview);
-//   imagePreview.src = imageUrl;
-//   imagePreview.alt = figCap;
-//   imageFigCaption.textContent = figCap;
-// }
-
-// export const showLikeCount = (likes, cardElement, userId) => {
-//   const likeButton = cardElement.querySelector('.element__like-button');
-//   const likeCounter = cardElement.querySelector('.element__like-count');
-
-//   likeCounter.textContent = `${likes.length}`;
-//   const isLike = likes.some((item) => item._id === userId);
-//   likeButton.classList.toggle('element__like-button_active', isLike);
-// }
-
-// // Функция создания карточек
-// export function createCard({name, link, likes, _id, owner}) {
-//   const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
-//   const image = cardElement.querySelector('.element__image');
-//   const heading = cardElement.querySelector('.element__heading');
-//   const removeButton = cardElement.querySelector('.element__delete-button');
-//   const userId = getId();
-//   cardElement.querySelector('.element__like-button').addEventListener('click', () => {
-//     updLikes(_id, userId, cardElement);
-//   });
-//   cardElement.querySelector('.element__delete-button').addEventListener('click', () => {
-//     api.deleteCard(_id)
-//     .then (() => {
-//       cardElement.remove()
-//     })
-//     .catch((error) => console.error(error))
-//   });
-//   heading.textContent = name;
-//   image.src = link;
-//   image.alt = name;
-
-//   if (owner._id !== userId) {
-//     removeButton.remove()
-//   }
-
-//   showLikeCount(likes, cardElement, userId)
-
-//   image.addEventListener('click', () => {
-//     openImagePreview(link, name)
-//   });
-//   return cardElement;
-// }
-
-// Функция добавления карточек
 export const addCard = (cards) => {
   cards.forEach((element) => {
-    console.log(element);
     cardsContainer.append(new Card(element, '#card-Template').generate())
   })
 };
