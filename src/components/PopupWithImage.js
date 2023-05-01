19743b1ba1ed294a06780647f9ed25221673017f
Vladimir Z <armgidosha@list.ru>
2023-05-01 10:12:36 +0300

    refactor: minor changes in structure

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,12 +3,12 @@ import Popup from "./Popup";
 export default class PopupWithImage extends Popup {
 	constructor(popup) {
 		super(popup);
+		this._imagePreview = this._popup.querySelector('.popup__image');
+		this._imageFigCaption = this._popup.querySelector('.popup__figcaption');
 	}
 
 	openPopup(data) {
 		super.openPopup();
-		this._imagePreview = this._popup.querySelector('.popup__image');
-		this._imageFigCaption = this._popup.querySelector('.popup__figcaption');
 
 		this._imagePreview.src = data.link;
 		this._imagePreview.alt = data.name;
